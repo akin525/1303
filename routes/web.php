891f14c8d014a852bcfba3d81471560638690f59
DELL <akinlabisamson15@gmail.com>
2024-05-13 12:08:46 +0100

    new

--- routes/web.php
+++ routes/web.php
@@ -502,7 +502,7 @@ Route::get('/migrate', function(){
 
 
 Route::get('/uploads/custom-images/{filename}', function ($filename) {
-    $path = storage_path('app/' . $filename);
+    $path = storage_path('app/uploads/custom-images/' . $filename);
 
 //    if (!File::exists($path)) {
 //        abort(404);
