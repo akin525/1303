8a85cf4d90092530880a84c1e000bf175290b037
DELL <akinlabisamson15@gmail.com>
2024-05-19 21:26:09 +0100

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use App\Models\Setting;
@@ -36,6 +37,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
         if (! $this->app->runningInConsole()) {
             $setting = Setting::first();
